10eb2d152af6571983d14bf6b9c0bf2f7951b8cb
saul <sa2023078332@virtual.upt.pe>
2025-09-16 17:24:12 -0500

    avance2

--- main.cpp
+++ main.cpp
@@ -1,9 +1,36 @@
 #include <GL/glut.h>
+#include <cstdlib>
 
 int winW = 800, winH = 600;
 
+bool showGrid = true;
+bool showAxes = true;
+int gridSpacing = 20;
+
+void drawGrid() {
+    glColor3ub(220, 220, 220);
+    glBegin(GL_POINTS);
+    for (int x = 0; x <= winW; x += gridSpacing) {
+        for (int y = 0; y <= winH; y += gridSpacing) {
+            glVertex2i(x, y);
+        }
+    }
+    glEnd();
+}
+
+void drawAxes() {
+    glColor3ub(150, 0, 0);
+    glBegin(GL_POINTS);
+    for (int x = 0; x <= winW; x++) glVertex2i(x, winH / 2);
+    for (int y = 0; y <= winH; y++) glVertex2i(winW / 2, y);
+    glEnd();
+}
+
 void display() {
     glClear(GL_COLOR_BUFFER_BIT);
+
+    if (showGrid) drawGrid();
+    if (showAxes) drawAxes();
 
     glColor3ub(255, 0, 0);
     glBegin(GL_POINTS);
@@ -11,6 +38,15 @@ void display() {
     glEnd();
 
     glutSwapBuffers();
+}
+
+void keyboard(unsigned char key, int, int) {
+    switch (key) {
+        case 'G': case 'g': showGrid = !showGrid; break;
+        case 'E': case 'e': showAxes = !showAxes; break;
+        case 27: exit(0);
+    }
+    glutPostRedisplay();
 }
 
 void reshape(int w, int h) {
@@ -27,13 +63,14 @@ int main(int argc, char** argv) {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
     glutInitWindowSize(winW, winH);
-    glutCreateWindow("Parte 1 - Ventana basica");
+    glutCreateWindow("Parte 2 - Grid y Ejes");
 
     glClearColor(1, 1, 1, 1);
-    glPointSize(4.0f);
+    glPointSize(2.0f);
 
     glutDisplayFunc(display);
     glutReshapeFunc(reshape);
+    glutKeyboardFunc(keyboard);
 
     glutMainLoop();
     return 0;
